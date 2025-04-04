c8e3d25a9ec5a26f5da3a575961749006025b233
Juan Albornoz <jualbornozc@gmail.com>
2025-04-04 18:27:13 -0300

    Se realizan cambios en la estructura de la consulta

--- Extracción de datos/scripts/codigos.py
+++ Extracción de datos/scripts/codigos.py
@@ -1,25 +1,26 @@
 import requests
 import pandas as pd
 
-# Extracción de capas geograficas disponibles por la api
-url_capas = "https://arclim.mma.gob.cl/api/capas"
+BASE_URL = "https://arclim.mma.gob.cl/api/"
+
+# Extracción de codigos de las capas geograficas disponibles por la api
+url_capas = BASE_URL + "capas"
 response_capas = requests.get(url_capas)
 capas = response_capas.json()
 
 df_capas = pd.DataFrame(capas)
 df_capas.to_csv("capas_api_arclim.csv", index=False)
 
-# Extracción de los indicadores disponibles por la api
-
-url_indicadores = "https://arclim.mma.gob.cl/api/indicadores_climaticos"
+# Extracción codigos de los indicadores disponibles por la api
+url_indicadores = BASE_URL + "indicadores_climaticos"
 response_indicadores = requests.get(url_indicadores)
 indicadores = response_indicadores.json()
 
 df_indicadores = pd.DataFrame(indicadores)
 df_indicadores.to_csv("indicadores_api_arclim.csv", index=False)
 
-# Extracción de atributos para la capa geografica "comunas"
-url_atributos_comunas = "https://arclim.mma.gob.cl/api/attributos/comunas/"
+# Extracción de codigos de los atributos disponibles para la capa geografica "comunas"
+url_atributos_comunas = BASE_URL + "attributos/comunas/"
 response_atributos_comunas = requests.get(url_atributos_comunas)
 atributos_comunas = response_atributos_comunas.json()
 
--- Extracción de datos/scripts/consultas.py
+++ Extracción de datos/scripts/consultas.py
@@ -1,23 +1,53 @@
 import requests
 import pandas as pd
-from funciones_extraccion import consulta_de_indicador, consulta_masiva, BASE_URL
-from convertir_a_df import convertir_a_DataFrame, convertir_masivo_a_DataFrame
+from funciones_extraccion import consulta_masiva, BASE_URL
+from convertir_a_df import convertir_masivo_a_DataFrame
 
+"""
+    La consulta ha sido estructurada, para obtener en primera instancia los indices y atributos de la capa geográfica "comunas",
+    y luego guardar estos indices en una lista, para que junto a una lista de los periodos de tiempo, pasarlos como parametros
+    a las funciones 'consulta_masiva' y 'convertir_masivo_a_DataFrame', y así extraer los datos y guardarlos en un archivo CSV 
 
-# Se extrae los datos de las comunas 
-url_datos_comunas = BASE_URL + "datos/comunas/json/?attributes=NOM_COMUNA,NOM_PROVIN,NOM_REGION,REGION"
-response_datos_comunas = requests.get(url_datos_comunas)
-datos_comunas = response_datos_comunas.json()
+"""
 
-# Se convierten los datos extraidos de las comunas en un dataframe
-df_atributos_comunas = pd.DataFrame(datos_comunas["data"]["values"], columns=datos_comunas["data"]["columns"], index=datos_comunas["data"]["index"])
-df_atributos_comunas.to_csv("Atributos comunas de Chile.csv" )
+# Se extraen los atributos de la capa geográfica comunas
+url_atributos_comunas = BASE_URL + "datos/comunas/json/?attributes=NOM_COMUNA,NOM_PROVIN,NOM_REGION,REGION"
+response_atributos_comunas = requests.get(url_atributos_comunas)
 
-# Se genera una lista con los codigos de las comunas 
-codigo_comunas = df_atributos_comunas.index.tolist()
-meses = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dec"] 
+# Si la respuesta es positiva, se pasa a generar un dataframe con los datos y guardarlos en un archivo CSV.
+if response_atributos_comunas.status_code == 200:
+    datos_comunas = response_atributos_comunas.json()
+    print(f"✅ Consulta de atributos de comunas ha sido exitosa")
 
+    df_atributos_comunas = pd.DataFrame(
+        datos_comunas["data"]["values"], 
+        columns=datos_comunas["data"]["columns"], 
+        index=datos_comunas["data"]["index"]
+        )
+    df_atributos_comunas.to_csv("Atributos comunas de Chile.csv" )
 
-pr_comunas = consulta_masiva("pr_sum", "comunas", codigo_comunas, meses )
-df_pr_comunas = convertir_masivo_a_DataFrame(pr_comunas, ["years", "mean"], True, "Datos precipitaciones comunas.csv")
+    # Se genera una lista con los códigos de las comunas para iterar en las consultas
+    codigo_comunas = df_atributos_comunas.index.tolist()
+    # Se genera una lista con los periodos de tiempo sobre los que se quiere iterar en las consultas
+    meses = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dec"] 
 
+    # Ejecución consulta de datos de temperaturas medias por comuna (mensual)
+    temp_comunas = consulta_masiva("mean_temperature", "comunas", codigo_comunas, meses )
+    df_temp_comunas = convertir_masivo_a_DataFrame(
+        temp_comunas,
+        ["years", "mean"],
+        exportar=True,
+        nombre_archivo="Datos temperaturas comunas.csv"
+    )
+
+    # Ejecución consulta de datos de precipitaciones acumuladas por comuna (mensual)
+    pr_comunas = consulta_masiva("pr_sum", "comunas", codigo_comunas, meses )
+    df_pr_comunas = convertir_masivo_a_DataFrame(
+        pr_comunas,
+        ["years", "mean"],
+        exportar=True,
+        nombre_archivo="Datos precipitaciones comunas.csv"
+    )   
+
+else:
+    print(f"❗ Error en la consulta de los atributos de comunas. Código de estado: {response_atributos_comunas.status_code}")
